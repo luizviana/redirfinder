1288f9c37376175ade104e32f3e8d3a4f505e7d3
Luiz P. Viana <46410487+luizviana@users.noreply.github.com>
2020-02-23 21:09:01 -0300

    fix2 it's working much better

--- redirfinder.py
+++ redirfinder.py
@@ -11,10 +11,10 @@ def openfile(var1):
     url_list = []
     try:
         with open(var1) as file:
-            decoded_lines = file.read()
-            lines = decoded_lines.split("\n")
+            lines = file.read()
+            lines = lines.split("\n")
             for line in lines:
-                if "=http" in line or "=/" in line or "=%2f" in line or "aHR0c" in line:
+                if "=/" in line or "=%2f" in line or (re.search(r'=(([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4})\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?)', line)):
                     if line.startswith("http"):
                         url_list.append(line)
                     else:
@@ -22,7 +22,7 @@ def openfile(var1):
 
 
         if len(url_list) > 0:
-            print("[ ! ] {} Possible Redirect Parameters Found".format(len(url_list)))
+            print("[ ! ] {} Redirect Parameters Found".format(len(url_list)))
             return url_list
         else:
             print("[ - ] Redirect Parameters Not Found")
@@ -36,20 +36,24 @@ def openfile(var1):
 def redirect_urls(url_list):
     urls_to_request = []
     for url in url_list:
-        url_value = re.search(r'([^=&?]+)=([^&]+)', url)
+
+        url_value = re.search(r'=(([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4})\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?)', url)
+        url_value2 = re.search(r'=(\/[-a-zA-Z0-9@:%_\+\.~]*)&?', url)
 
         for payload in PAYLOADS:
-            url_to_request = url.replace("="+url_value.group(2), "="+payload)
+            url_to_request = url.replace("="+url_value.group(1), "="+payload)
             urls_to_request.append(url_to_request)
 
-        url_value2 = re.search(r'^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)', url)
+            url_to_request2 = url.replace("="+url_value2.group(1), "="+payload)
+            urls_to_request.append(url_to_request2)
 
         for payload in PAYLOADS2:
-            url_to_request2 = url.replace("="+url_value.group(2), ("="+payload + url_value2.group(1)))
-            urls_to_request.append(url_to_request2)
+            url_to_request3 = url.replace("="+url_value.group(1), ("="+payload + url_value.group(1).replace("http://", "").replace("https://", "")))
+            urls_to_request.append(url_to_request3)
 
     #Remove duplicates
     urls_to_request = list(dict.fromkeys(urls_to_request))
+    print(urls_to_request)
     return urls_to_request
 
 
